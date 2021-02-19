cf9618c65c60b07dd43db5c5fdbbac98a635b87f
AlexVee <alexveez@yandex.ru>
2021-02-19 12:35:47 +0300

    Example notes

---
+++ app/src/main/java/com/example/fragment_example/CurrentNoteThree.java
@@ -0,0 +1,28 @@
+package com.example.fragment_example;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class CurrentNoteThree extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_current_note_three, container, false);
+    }
+}
